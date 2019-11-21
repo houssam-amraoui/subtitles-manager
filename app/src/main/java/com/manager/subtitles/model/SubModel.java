83a069dd3d2cc90941e35681f59fe78ba0175a1a
houssam <houss_ammino@live.fr>
2019-11-21 09:39:40 +0100

    add entity in model enly

---
+++ app/src/main/java/com/manager/subtitles/model/SubModel.java
@@ -0,0 +1,10 @@
+package com.manager.subtitles.model;
+
+import java.util.ArrayList;
+
+public class SubModel {
+    int id;
+    SubTime timeStart;
+    SubTime timeEnd;
+    ArrayList<String> lines;
+}
---
+++ app/src/main/java/com/manager/subtitles/model/SubTime.java
@@ -0,0 +1,10 @@
+package com.manager.subtitles.model;
+
+public class SubTime {
+    int heurs; //2
+    int min;//2
+    int secend;//2
+    int mlsecend;//3
+
+
+}
